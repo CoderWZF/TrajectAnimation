dd249030820a2c703ffd6a376bce65fe3e30dab9
吴再锋 <1063867682@qq.com>
2018-06-12 11:18:16 +0800

    no message

--- TrajectoryAnimationDemo/TrajectoryAnimationDemo/ViewController.m
+++ TrajectoryAnimationDemo/TrajectoryAnimationDemo/ViewController.m
@@ -18,17 +18,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-//    [UIBezierPath bezierPathWithRoundedRect:self.demoView.bounds cornerRadius:self.cornerRadius]
+
+    
 }
 
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    TrajectoryView *view = [[TrajectoryView alloc] initWithParentView:_demoView];
-    view.borderColor = [UIColor redColor];
-    view.borderWidth = 10;
-    view.animationDuration = 2;
-    [view startAnimation];
+    TrajectoryView *trajectoryView = [[TrajectoryView alloc] initWithParentView:_demoView];
+    trajectoryView.borderColor = [UIColor redColor];
+    trajectoryView.borderWidth = 10;
+    trajectoryView.animationDuration = 2;
+    [trajectoryView startAnimation];
+    
+    //模拟2s后，停止动画
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [trajectoryView stopAnimation];
+    });
 }
 
 
